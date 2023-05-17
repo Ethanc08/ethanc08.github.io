fe64cee58c47b23f81e50a6a3535dd22dbe4b538
Ethanc08 <131011735+Ethanc08@users.noreply.github.com>
2023-05-17 08:36:13 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,15 @@
-// Generate a random number between 1 and 100
-const targetNumber = Math.floor(Math.random() * 100) + 1;
-let numberOfGuesses = 0;
-let guessedNumber = 0;
+let targetNumber;
+let numberOfGuesses;
+let guessedNumber;
+
+// Function to start a new game
+function startNewGame() {
+  // Generate a random number between 1 and 100
+  targetNumber = Math.floor(Math.random() * 100) + 1;
+  numberOfGuesses = 0;
+  guessedNumber = 0;
+  showResult('');
+}
 
 // Function to handle the user's guess
 function handleGuess() {
@@ -15,6 +23,8 @@ function handleGuess() {
   // Check if the guessed number is correct
   if (guessedNumber === targetNumber) {
     showResult(`Congratulations! You guessed the number in ${numberOfGuesses} guesses.`);
+    // Disable the guess button
+    guessButton.disabled = true;
   } else if (guessedNumber < targetNumber) {
     showResult('Too low! Guess higher.');
   } else {
@@ -34,3 +44,6 @@ function showResult(message) {
 // Attach the handleGuess function to the button's click event
 const guessButton = document.getElementById('guessButton');
 guessButton.addEventListener('click', handleGuess);
+
+// Start a new game when the page loads
+startNewGame();
